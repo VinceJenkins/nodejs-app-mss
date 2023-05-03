ab92bb15e8a4ddd7b859826dec2a6d616a46c904
VinceJenkins <114426417+VinceJenkins@users.noreply.github.com>
2023-05-03 15:47:54 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_5ec2e6e61bc4ae6a29b3803f12affd0d34966025',
+	    'sonar.login': 'squ_2c0dbf8cc21f12736d92c1377cfa772dfa7e0a4c',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
